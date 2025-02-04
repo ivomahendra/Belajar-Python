8b1ed05a66cfea7e714e13f6df4ffb211d54a238
ivomahendra <ivo.click@gmail.com>
2025-02-04 19:20:40 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,29 +1,19 @@
-numb1=float(input("Masukkan angka 1 : "))
-op=input("operasi +,-,*,/: ")
-numb2=float(input("Masukkan angka 2 : "))
-if op == "+" :
-   print("hasil penjumlahan : ",numb1+numb2)
-elif op == "-" :
-    print("Hasil pengurangan : ",numb1-numb2)
-elif op == "*" :
-    print("hasil perkalian : ",numb1*numb2)
-elif op == "/" :
-    print("Hasil pembagian : ",numb1/numb2)
-else :
-    print("masukkan operasi sesuai format")
+def main():
+    print('======Kalkulator======')
+    
 
-except ValueError:
-print("Invalid Input, please input only numbers")
-
-restart = input("Do you want to CALCULATE again? : ")
-
-
-if restart == "yes":
-
+while True:
     main()
-
-else:
-    print("Thanks! for calculating keep learning! hope you have a good day :)")
-
-
-    exit()
+    numb1=float(input("Masukkan angka 1 : "))
+    op=input("operasi +,-,*,/: ")
+    numb2=float(input("Masukkan angka 2 : "))
+    if op == "+" :
+        print("hasil penjumlahan : ",numb1+numb2,'\n')
+    elif op == "-" :
+        print("Hasil pengurangan : ",numb1-numb2,'\n')
+    elif op == "*" :
+        print("hasil perkalian : ",numb1*numb2,'\n')
+    elif op == "/" :
+        print("Hasil pembagian : ",numb1/numb2,'\n')
+    else :
+        print("masukkan operasi sesuai format")
